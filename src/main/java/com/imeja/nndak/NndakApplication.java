28df1bd8b3ff6ef3ea91ca9f0ec5e95de9a13482
Itskiprotich <keeprawteachjapheth@gmail.com>
2022-01-25 13:04:24 +0300

    initial setup

---
+++ src/main/java/com/imeja/nndak/NndakApplication.java
@@ -0,0 +1,13 @@
+package com.imeja.nndak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NndakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NndakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imeja/nndak/NndakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imeja.nndak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NndakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
